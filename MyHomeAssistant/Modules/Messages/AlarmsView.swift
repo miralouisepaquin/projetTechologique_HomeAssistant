56dbb4499f3c223da734b6ff1f72005f135e9a82
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2023-04-28 14:48:21 -0400

    Ajout suivit du Topic
    Ajout suivit du Topic lors du retour page setting

--- MyHomeAssistant/Modules/Messages/AlarmsView.swift
+++ MyHomeAssistant/Modules/Messages/AlarmsView.swift
@@ -1,3 +1,9 @@
+//
+//  AlarmsView.swift
+//  MySchoolAssistant
+//
+//  Created by Mira-Louise Paquin on 2023-03-16.
+//
 import SwiftUI
 
 struct AlarmsView: View {
@@ -18,23 +24,12 @@ struct AlarmsView_Previews: PreviewProvider {
 }
 
 struct AlarmView: View {
-    @State var topic: String = ""
-    @State var message: String = ""
     @EnvironmentObject private var mqttManager: MQTTManager
-    @StateObject private var apiManager = APIManager()
+    @EnvironmentObject private var apiManager: APIManager
     var body: some View {
         VStack {
             ConnectionStatusBar(message: mqttManager.connectionStateMessage(), isConnected: mqttManager.isConnected())
             VStack {
-                HStack {
-                    MQTTTextField(placeHolderMessage: "Enter a topic to subscribe", isDisabled: !mqttManager.isConnected() || mqttManager.isSubscribed(), message: $topic)
-                    Button(action: functionFor(state: mqttManager.currentAppState.appConnectionState)) {
-                        Text(titleForSubscribButtonFrom(state: mqttManager.currentAppState.appConnectionState))
-                            .font(.system(size: 14.0))
-                    }.buttonStyle(BaseButtonStyle(foreground: .white, background: .green))
-                        .frame(width: 100)
-                        .disabled(!mqttManager.isConnected() || topic.isEmpty)
-                }.padding(.bottom, 30)
                 VStack {
                     ForEach(apiManager.currentSensorState.listItems, id:\.self){ sensor in
                         AlarmContainer(sensor: sensor, isDetected: mqttManager.currentSensorState.sensorConnectionState.isDetected && sensor == mqttManager.currentSensorState.sensorName)
@@ -46,7 +41,7 @@ struct AlarmView: View {
         }
         .navigationTitle("Alarm")
         .navigationBarItems(trailing: NavigationLink(
-            destination: SettingsView(brokerAddress: mqttManager.currentHost() ?? ""),
+            destination: SettingsView(brokerAddress: mqttManager.currentHost() ?? "",topic: mqttManager.currentTopic() ?? ""),
             label: {
                 Image(systemName: "gear")
             }))
@@ -54,32 +49,6 @@ struct AlarmView: View {
         .onAppear {
             apiManager.currentSensorState.clearSensorList()
             apiManager.getSensorsRequest()
-        }
-    }
-
-    private func subscribe(topic: String) {
-        mqttManager.subscribe(topic: topic)
-    }
-
-    private func usubscribe() {
-        mqttManager.unSubscribeFromCurrentTopic()
-    }
-    
-    private func titleForSubscribButtonFrom(state: MQTTAppConnectionState) -> String {
-        switch state {
-        case .connected, .connectedUnSubscribed, .disconnected, .connecting:
-            return "Subscribe"
-        case .connectedSubscribed:
-            return "Unsubscribe"
-        }
-    }
-
-    private func functionFor(state: MQTTAppConnectionState) -> () -> Void {
-        switch state {
-        case .connected, .connectedUnSubscribed, .disconnected, .connecting:
-            return { subscribe(topic: topic) }
-        case .connectedSubscribed:
-            return { usubscribe() }
         }
     }
 }
