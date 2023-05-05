c53f56d521f532fb89e8255b76a144927407f807
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2023-05-05 09:31:46 -0400

    Ajout des langues
    ajout des switch pour gérer les boutons et autres texte non gérer par les fichier TextFiles

--- MyHomeAssistant/Modules/Settings/SettingsView.swift
+++ MyHomeAssistant/Modules/Settings/SettingsView.swift
@@ -1,25 +1,44 @@
 //
 //  SettingsView.swift
-//  MyHomeAssistant
+//  MySchoolAssistant
 //
 //  Created by Mira-Louise Paquin on 2023-03-16.
 //
 import SwiftUI
 
 struct SettingsView: View {
+    let langStr = Locale.preferredLanguages[0]
     @State var brokerAddress: String = ""
+    @State var topic: String = ""
+    @State var message: String = ""
     @EnvironmentObject private var mqttManager: MQTTManager
     var body: some View {
         VStack {
             ConnectionStatusBar(message: mqttManager.connectionStateMessage(), isConnected: mqttManager.isConnected())
-            MQTTTextField(placeHolderMessage: "Enter broker Address", isDisabled: mqttManager.currentAppState.appConnectionState != .disconnected, message: $brokerAddress)
-                .padding(EdgeInsets(top: 0.0, leading: 7.0, bottom: 0.0, trailing: 7.0))
+            if langStr == "en"{
+                MQTTTextField(placeHolderMessage: "Enter broker Address", isDisabled: mqttManager.currentAppState.appConnectionState != .disconnected, message: $brokerAddress)
+                    .padding(EdgeInsets(top: 0.0, leading: 7.0, bottom: 0.0, trailing: 7.0))
+            }else{
+                MQTTTextField(placeHolderMessage: "Entrez l'adresse du broker", isDisabled: mqttManager.currentAppState.appConnectionState != .disconnected, message: $brokerAddress)
+                    .padding(EdgeInsets(top: 0.0, leading: 7.0, bottom: 0.0, trailing: 7.0))
+            }
             HStack(spacing: 50) {
                 setUpConnectButton()
                 setUpDisconnectButton()
             }
+            HStack {
+                if langStr == "en"{
+                    MQTTTextField(placeHolderMessage: "Enter a topic to subscribe", isDisabled: !mqttManager.isConnected() || mqttManager.isSubscribed(), message: $topic)
+                }else{
+                    MQTTTextField(placeHolderMessage: "Entrez votre Topic", isDisabled: !mqttManager.isConnected() || mqttManager.isSubscribed(), message: $topic)
+                }
+                setUpSubscribeButton()
+            }.padding(.bottom, 30)
             .padding()
             Spacer()
+            HStack {
+                
+            }
         }
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.inline)
@@ -39,6 +58,16 @@ struct SettingsView: View {
         }.buttonStyle(BaseButtonStyle(foreground: .white, background: .red))
         .disabled(mqttManager.currentAppState.appConnectionState == .disconnected)
     }
+    
+    private func setUpSubscribeButton() -> some View  {
+        return Button(action: functionFor(state: mqttManager.currentAppState.appConnectionState)) {
+            Text(titleForSubscribButtonFrom(state: mqttManager.currentAppState.appConnectionState))
+                .font(.system(size: 14.0))
+        }.buttonStyle(BaseButtonStyle(foreground: .white, background: .green))
+            .frame(width: 100)
+            .disabled(!mqttManager.isConnected() || topic.isEmpty)
+    }
+    
     private func configureAndConnect() {
         // Initialize the MQTT Manager
         mqttManager.initializeMQTT(host: brokerAddress, identifier: UUID().uuidString)
@@ -48,6 +77,41 @@ struct SettingsView: View {
 
     private func disconnect() {
         mqttManager.disconnect()
+    }
+    
+    private func subscribe(topic: String) {
+        mqttManager.subscribe(topic: topic)
+    }
+
+    private func usubscribe() {
+        mqttManager.unSubscribeFromCurrentTopic()
+    }
+    
+    private func titleForSubscribButtonFrom(state: MQTTAppConnectionState) -> String {
+        if langStr == "en"{
+            switch state {
+            case .connected, .connectedUnSubscribed, .disconnected, .connecting:
+                return "Subscribe"
+            case .connectedSubscribed:
+                return "Unsubscribe"
+            }
+        }else{
+            switch state {
+            case .connected, .connectedUnSubscribed, .disconnected, .connecting:
+                return "Connexion"
+            case .connectedSubscribed:
+                return "Déconnexion"
+            }
+        }
+    }
+
+    private func functionFor(state: MQTTAppConnectionState) -> () -> Void {
+        switch state {
+        case .connected, .connectedUnSubscribed, .disconnected, .connecting:
+            return { subscribe(topic: topic) }
+        case .connectedSubscribed:
+            return { usubscribe() }
+        }
     }
 }
 
